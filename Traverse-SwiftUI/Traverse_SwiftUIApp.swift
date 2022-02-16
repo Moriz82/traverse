41c721fc09244e57d2c3d057422864f4f22b01f7
aidenseibel <aiden.seibel@gmail.com>
2022-02-16 15:51:40 -0600

    Initial Commit

---
+++ Traverse-SwiftUI/Traverse_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traverse_SwiftUIApp.swift
+//  Traverse-SwiftUI
+//
+//  Created by Aiden Seibel on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Traverse_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
